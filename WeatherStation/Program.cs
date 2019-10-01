6a2222e59286cbfd58eded6ddeef66b8de592e66
Clemens Schneider <clemens.schneider@posteo.de>
2019-10-01 18:42:58 +0200

    Extended Program file

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -10,14 +10,46 @@ namespace WeatherStation
     {
         static void Main()
         {
+            ShowWeather();
+            //ShowTemperature();
+            //ShowPressure();
+            //ShowHumidity();
+
+            Console.ReadKey();
+        }
+
+        static void ShowWeather()
+        {
             WeatherController weather = new WeatherController(
                 new HumidityModel(new HumiditySensorAbstraction()),
                 new TemperatureModel(new TemperatureSensorAbstraction()),
                 new PressureModel(new PressureSensorAbstraction()));
 
             weather.Display(new MainView(new Output.ConsoleOutput()));
+        }
 
-            Console.ReadKey();
+        static void ShowTemperature()
+        {
+            TemperatureController temperature = new TemperatureController(
+                new TemperatureModel(new TemperatureSensorAbstraction()));
+
+            temperature.Display(new TemperatureView(new Output.ConsoleOutput()));
+        }
+
+        static void ShowPressure()
+        {
+            PressureController pressure = new PressureController(
+                new PressureModel(new PressureSensorAbstraction()));
+
+            pressure.Display(new PressureView(new Output.ConsoleOutput()));
+        }
+
+        static void ShowHumidity()
+        {
+            HumidityController temperature = new HumidityController(
+                new HumidityModel(new HumiditySensorAbstraction()));
+
+            temperature.Display(new HumidityView(new Output.ConsoleOutput()));
         }
     }
 }
