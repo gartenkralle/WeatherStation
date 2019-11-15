caa55034fdc60a8ace00cb796b23496dd1aec0ef
Clemens Schneider <clemens.schneider@posteo.de>
2019-11-15 11:06:17 +0100

    Improved naming

--- Program/WindowsFormsApp/Properties/Settings.Designer.cs
+++ Program/WindowsFormsApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp.Properties
+namespace Program.WindowsFormsApp.Properties
 {
 
 
